c50df3b7f1335dd585960d8c20acc00ae289726b
Devika Lakshmanan <46183395+DevikaLak@users.noreply.github.com>
2019-02-19 08:26:00 -0500

    Add files via upload

---
+++ choose_your_own/your_algorithm.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+### the training data (features_train, labels_train) have both "fast" and "slow"
+### points mixed together--separate them so we can give them different colors
+### in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+"""
+#### initial visualization
+plt.xlim(0.0, 1.0)
+plt.ylim(0.0, 1.0)
+plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.legend()
+plt.xlabel("bumpiness")
+plt.ylabel("grade")
+plt.show()
+################################################################################
+"""
+
+### your code here!  name your classifier object clf if you want the 
+### visualization code (prettyPicture) to show you the decision boundary
+
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC 
+import sys
+from time import time
+import math
+
+myClassifier1 = RFC(n_estimators=1000,criterion="entropy",min_samples_split=80,max_features=None,n_jobs=-1)
+
+startTrainingTime = time()
+myClassifier1.fit(features_train,labels_train)
+print "The training time is: ",round(time()-startTrainingTime,3),"seconds"
+
+startPredictionTime = time()
+myPredictions1 = myClassifier1.predict(features_test)
+print "The prediction time is: ",round(time()-startPredictionTime,3),"seconds"
+
+accuracy1 = accuracy_score(labels_test,myPredictions1)
+print "Accuracy is: ",accuracy1
+
+
+myClassifier2 = SVC(C=100000.0,kernel = "rbf")
+
+startTrainingTime = time()
+myClassifier2.fit(features_train,labels_train)
+print "The training time is: ",round(time()-startTrainingTime,3),"seconds"
+
+startPredictionTime = time()
+myPredictions2 = myClassifier2.predict(features_test)
+print "The prediction time is: ",round(time()-startPredictionTime,3),"seconds"
+
+accuracy2 = accuracy_score(labels_test,myPredictions2)
+print "Accuracy is: ",accuracy2
+
+
+"""
+try:
+    prettyPicture(myClassifier, features_test, labels_test)
+except NameError:
+    pass
+"""
